a7d4db10af36f143534d9d8cb47d0fdbf74af646
NameOfTheLaw <tsibin.andr@gmail.com>
2017-03-28 00:04:58 +0300

    added next excercise button

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,4 @@ var editor = ace.edit("code-editor");
 editor.getSession().setMode("ace/mode/javascript");
 editor.setFontSize(20);
 editor.setValue('regexp = "type_your_regular_expression_here";');
+editor.clearSelection();
