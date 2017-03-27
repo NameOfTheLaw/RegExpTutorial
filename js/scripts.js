224e2d4e1e1ef567e7123662b1ae1d9f10736097
NameOfTheLaw <tsibin.andr@gmail.com>
2017-03-27 23:46:14 +0300

    added ace editor

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var editor = ace.edit("code-editor");
+editor.getSession().setMode("ace/mode/javascript");
+editor.setFontSize(20);
+editor.setValue('regexp = "type_your_regular_expression_here";');
