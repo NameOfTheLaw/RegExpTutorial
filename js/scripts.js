ef5ce51090f7d34ffdb53c11fbee0ae05b7032ab
NameOfTheLaw <tsibin.andr@gmail.com>
2017-03-29 00:03:43 +0300

    replace ace editor with input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+/*
 var editor = ace.edit("code-editor");
 editor.getSession().setMode("ace/mode/javascript");
 editor.setFontSize(20);
 editor.setValue('regexp = "type_your_regular_expression_here";');
 editor.clearSelection();
+*/
